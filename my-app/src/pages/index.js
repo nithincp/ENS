c5fe2bab8151297e493c6d00ac47946a3f3b011b
Nithin Chandran P <p.nithinc@gmail.com>
2023-04-12 16:29:33 +0530

    Changed the function name to Home

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -4,7 +4,7 @@ import Web3Modal from "web3modal"
 import { ethers,providers } from "ethers"
 import { useEffect,useRef,useState } from "react"
 
-export default function main(){
+export default function Home(){
   
   const [walletConnected,setWalletConnected] = useState(false)
   const web3ModalRef = useRef()
